606127af98c55a9a1469b52aae595942ccbcd010
dircksens <marianadircksen13@gmail.com>
2023-03-01 20:14:26 -0300

    .

--- index.php
+++ index.php
@@ -7,69 +7,24 @@
     </head>
     <body>
         <?php
-         //A       
+        //Calcular 
          $x =6;
          $y = 2;
          
+         echo '<h2>Cálculo</h2>';
          echo '<div>' .($x+$y). '</div>';
          echo '<div>' .($x-$y). '</div>';
          echo '<div>' .($x*$y). '</div>';
          echo '<div>' .($x/$y). '</div>';
          
-         //B
+        //nome 
          echo '<br/>';
          $nome = 'Mariana';
          $sobrenome = 'Dircksen';
          
          echo '<div>'.$nome,' ',$sobrenome.'</div>';
          
-         //C
-         echo '<br/>';
-         const V_PI = 3.14;
-         
-         $raio = 2.13;
-         $a =  V_PI* ($raio*$raio);
-         
-         echo '<div>'.$a.'</div.';
-     
-        //D
-         echo '<br/>';
-         $carteira = 150;
-         $salario = 1500;
-         $salario += $carteira;
-         
-         echo '<div>'.$salario.'</div>';
-         
-        //E
-         $idade = 20;
-         $idade ++;
-         $idade ++;
-         
-         echo '<div>'.$idade.'</div>';
-         
-         //F
-         
-          $p_nomes = [
-              'Goku', 
-              'Nami',
-              'Frezza',
-              'Luffy'
-              ];
-         
-           echo '<pre>';
-           var_dump($p_nomes);
-          echo '</pre>';
-          
-          echo $p_nomes[0]; 
-          
-          $p_nomes[4] = 'Bills';
-          
-          echo '<pre>';
-           var_dump($p_nomes);
-          echo '</pre>';
-          
-          echo $p_nomes[4];
-          
+       
           
         ?>
     </body>
